50cc778493e9a05a4a746223eaf0ef23d511e6bd
daopopyo-blip <daopopyo@gmail.com>
2026-05-18 01:06:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -51,6 +51,9 @@ function subscribeNews() {
     document.getElementById('newsEmail').value = "";
 }
 
+    alert("✅ Спасибо! Вы подписаны на новости.");
+    document.getElementById('newsEmail').value = "";
+
 // КНОПКА "НАВЕРХ"
 window.addEventListener('scroll', function() {
     let goTop = document.querySelector('.go-top');
